74678f40b22720a54aca47d0ba4ccb129ba2f6f6
wxianxin <wxianxinreg@gmail.com>
2019-07-14 17:41:04 -0400

    Add greeks: Delta, Vega, Rho, Theta, Gamma

--- quantool/option.py
+++ quantool/option.py
@@ -4,7 +4,6 @@
         Vega
         Theta
         Rho
-        Lambda
         Epsilon
     2nd order:
         Gamma
@@ -29,6 +28,7 @@ class Option(object):
         r: float,
         tau: float,
         sigma: float = np.nan,
+        d: float = 0,
         v: float = np.nan,
     ):
         """"""
@@ -38,7 +38,9 @@ class Option(object):
         self.r = r
         self.tau = tau
         self.sigma = sigma
+        self.d = d
         self.v = v
+        self.greek_dict = {}
 
     def get_price(self):
         """TODO test"""
@@ -49,11 +51,11 @@ class Option(object):
         if self.option_type == "C":
             self.v = (
                 norm.cdf(d1) * self.s
-                - norm.cdf(d2) * math.e ** (-self.r * self.tau) * self.k
+                - norm.cdf(d2) * math.exp(-self.r * self.tau) * self.k
             )
         elif self.option_type == "P":
             self.v = (
-                norm.cdf(-d2) * math.e ** (-self.r * self.tau) * self.k
+                norm.cdf(-d2) * math.exp(-self.r * self.tau) * self.k
                 - norm.cdf(-d1) * self.s
             )
         else:
@@ -63,5 +65,56 @@ class Option(object):
 
     def get_greeks(self):
         """"""
-        pass
+        d1 = (
+            math.log(self.s / self.k) + (self.r + 0.5 * self.sigma ** 2) * self.tau
+        ) / (self.sigma * self.tau)
+        d2 = d1 - self.sigma * self.tau
+        if self.option_type == "C":
+            self.greek_dict["delta"] = math.exp(-self.d * self.tau) * norm.cdf(d1)
+            self.greek_dict["vega"] = (
+                self.k * math.exp(-self.r * self.tau) * norm.pdf(d2) * self.tau ** 0.5
+            )
+            self.greek_dict["theta"] = (
+                -math.exp(-self.d * self.tau)
+                * self.s
+                * norm.pdf(d1)
+                * self.sigma
+                / (2 * self.tau ** 0.5)
+                - self.r * self.k * math.exp(-self.r * self.tau) * norm.cdf(d2)
+                + self.d * self.s * math.exp(-self.d * self.tau) * norm.cdf(d1)
+            )
+            self.greek_dict["rho"] = (
+                self.k * self.tau * math.exp(-self.r * self.tau) * norm.cdf(d2)
+            )
+            self.greek_dict["gamma"] = (
+                self.k
+                * math.exp(-self.r * self.tau)
+                * norm.pdf(d2)
+                / (self.s ** 2 * self.sigma * self.tau ** 0.5)
+            )
 
+        elif self.option_type == "P":
+            self.greek_dict["delta"] = -math.exp(-self.d * self.tau) * norm.cdf(-d1)
+            self.greek_dict["vega"] = (
+                self.k * math.exp(-self.r * self.tau) * norm.pdf(d2) * self.tau ** 0.5
+            )
+            self.greek_dict["theta"] = (
+                -math.exp(-self.d * self.tau)
+                * self.s
+                * norm.pdf(d1)
+                * self.sigma
+                / (2 * self.tau ** 0.5)
+                + self.r * self.k * math.exp(-self.r * self.tau) * norm.cdf(-d2)
+                - self.d * self.s * math.exp(-self.d * self.tau) * norm.cdf(-d1)
+            )
+            self.greek_dict["rho"] = (
+                -self.k * self.tau * math.exp(-self.r * self.tau) * norm.cdf(-d2)
+            )
+            self.greek_dict["gamma"] = (
+                self.k
+                * math.exp(-self.r * self.tau)
+                * norm.pdf(d2)
+                / (self.s ** 2 * self.sigma * self.tau ** 0.5)
+            )
+
+        return self.greek_dict
--- quantool/tests/test_option.py
+++ quantool/tests/test_option.py
@@ -5,20 +5,46 @@ __author__ = "Steven Wang"
 import quantool
 
 option_data = {
-    "option_type": "P",
+    "option_type": "C",
     "s": 100,
     "k": 100,
     "r": 0.05,
     "tau": 1,
     "sigma": 0.25,
+    "d": 0,
     "v": 0,
+}
+
+benchmark = {
+    "C_price": 12.33599893,
+    "P_price": 7.45894138,
+    "C_greeks": {
+        "delta": 0.62740946,
+        "vega": 37.84198319,
+        "theta": -7.25049527,
+        "rho": 50.40494748,
+        "gamma": 0.01513679,
+    },
+    "P_greeks": {
+        "delta": -0.37259054,
+        "vega": 37.84198319,
+        "theta": -2.49434815,
+        "rho": -44.71799497,
+        "gamma": 0.01513679,
+    },
 }
 
 
 def test_get_price():
-    option = quantool.option.Option(**option_data)
+    option = quantool.Option(**option_data)
     print(option.get_price())
+
+
+def test_get_greeks():
+    option = quantool.Option(**option_data)
+    print(option.get_greeks())
 
 
 if __name__ == "__main__":
     test_get_price()
+    test_get_greeks()
