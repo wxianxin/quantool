141e6fc9f84a10d5bd58fc9a79f229f4b8746048
wxianxin <wxianxinreg@gmail.com>
2020-07-14 00:00:22 -0400

    update structure; prepare for multiline output

--- quantool/__init__.py
+++ quantool/__init__.py
@@ -4,6 +4,8 @@ __author__ = "Steven Wang"
 
 from .option import Option
 from .watch_market import watch_market
+# from pkgutil import extend_path
+# __path__ = extend_path(__path__, __name__)
 
 
 if __name__ == "__main__":
--- quantool/watch_market.py
+++ quantool/watch_market.py
@@ -2,22 +2,90 @@
 
 import datetime
 import requests
+import pkgutil
 import time
 import yaml
 
 
+def print_row(
+    snapshot_time: str,
+    stdout_conf_dict: dict,
+    conf_dict: dict,
+    up_down_mask: list,
+    row_dict: dict,
+    row_mask_list: dict,
+    row_position: str,
+):
+    """"""
+    selected_row_dict = {x: row_dict[x] for x in row_mask_list if x in row_mask_list}
+    selected_up_down_mask = {
+        x: up_down_mask[x] for x in row_mask_list if x in row_mask_list
+    }
+    print(
+        snapshot_time
+        + " "
+        + "".join(
+            "{:>18}".format(
+                stdout_conf_dict[conf_dict["color_mapping"][selected_up_down_mask[_]]]
+                + str(x)
+                + stdout_conf_dict["reset"]
+            )
+            for _, x in selected_row_dict.items()
+        ),
+        end=row_position,
+    )
+
+
+def print_table(
+    snapshot_time: str,
+    stdout_conf_dict: dict,
+    conf_dict: dict,
+    up_down_mask: list,
+    row_dict: dict,
+):
+    """"""
+    for row_num in conf_dict["symbol_dict"].keys():
+        print_row(
+            " " * 17,
+            stdout_conf_dict,
+            conf_dict,
+            up_down_mask,
+            {x: x for x in row_dict.keys()},
+            conf_dict["symbol_dict"][row_num],
+            "\n",
+        )
+        print_row(
+            snapshot_time,
+            stdout_conf_dict,
+            conf_dict,
+            up_down_mask,
+            row_dict,
+            conf_dict["symbol_dict"][row_num],
+            "\n",
+        )
+
+
 def watch_market(interval: int = 2):
     """"""
+
+    data = pkgutil.get_data(__name__, "watch_market.yaml")
+    conf_dict = yaml.safe_load(data)
+    data = pkgutil.get_data(__name__, "stdout_conf.yaml")
+    stdout_conf_dict = yaml.safe_load(data)
+
     with open("watch_market.yaml", "r") as f:
         conf_dict = yaml.safe_load(f)
     with open("stdout_conf.yaml", "r") as f:
         stdout_conf_dict = yaml.safe_load(f)
 
+    symbol_list = []
+    for row in conf_dict["symbol_dict"].values():
+        symbol_list += row
     field_string = ",".join(conf_dict["field_list"])
-    symbol_string = ",".join(conf_dict["symbol_list"])
+    symbol_string = ",".join(symbol_list)
 
-    snapshot_dict = {1: {x: 0 for x in conf_dict["symbol_list"]}}
-    up_down_mask = {x: 0 for x in conf_dict["symbol_list"]}
+    snapshot_dict = {1: {x: 0 for x in symbol_list}}
+    up_down_mask = {x: 0 for x in symbol_list}
 
     session = requests.Session()
     while True:
@@ -36,7 +104,7 @@ def watch_market(interval: int = 2):
             x["symbol"]: x["regularMarketPrice"]
             for x in resp.json()["quoteResponse"]["result"]
         }
-        for _ in conf_dict["symbol_list"]:
+        for _ in symbol_list:
             diff = snapshot_dict[1][_] - snapshot_dict[0][_]
             if diff > 0:
                 up_down_mask[_] = 1
@@ -44,30 +112,14 @@ def watch_market(interval: int = 2):
                 up_down_mask[_] = -1
             else:
                 up_down_mask[_] = 0
-        print(
-            " " * 21
-            + "".join(
-                "{:20}".format(
-                    stdout_conf_dict["bold"] + str(x) + stdout_conf_dict["reset"]
-                )
-                for x in snapshot_dict[1].keys()
-            )
-        )
-        print(
-            snapshot_time
-            + " " * 4
-            + "".join(
-                "{:20}".format(
-                    stdout_conf_dict[conf_dict["color_mapping"][up_down_mask[_]]]
-                    + str(x)
-                    + stdout_conf_dict["reset"]
-                )
-                for _, x in snapshot_dict[1].items()
-            )
+
+        print_table(
+            snapshot_time, stdout_conf_dict, conf_dict, up_down_mask, snapshot_dict[1],
         )
 
         time.sleep(interval)
 
 
 if __name__ == "__main__":
+    __name__ = "quantool"
     watch_market()
