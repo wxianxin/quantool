09df3d97ddd9e38af108fe51b472a8ca60a00008
wxianxin <wxianxinreg@gmail.com>
2020-07-14 20:34:47 -0400

    update row display logic

--- quantool/__init__.py
+++ quantool/__init__.py
@@ -4,6 +4,7 @@ __author__ = "Steven Wang"
 
 from .option import Option
 from .watch_market import watch_market
+
 # from pkgutil import extend_path
 # __path__ = extend_path(__path__, __name__)
 
--- quantool/watch_market.py
+++ quantool/watch_market.py
@@ -14,25 +14,35 @@ def print_row(
     up_down_mask: list,
     row_dict: dict,
     row_mask_list: dict,
-    row_position: str,
+    row_position: int,
+    row_count: int,
+    style: str,
 ):
-    """"""
+    """
+    NOTE: python format padding also takes ANSII escape characters into account
+    """
     selected_row_dict = {x: row_dict[x] for x in row_mask_list if x in row_mask_list}
     selected_up_down_mask = {
         x: up_down_mask[x] for x in row_mask_list if x in row_mask_list
     }
+
+    if row_position == 1:
+        print(
+            f"{stdout_conf_dict['line_start']}{row_count + 2}A{(' ' + snapshot_time + ' ').center(90, '#')}"
+        )
+    snapshot_time = " " * 17
     print(
-        snapshot_time
-        + " "
+        stdout_conf_dict["line_clear"]
         + "".join(
-            "{:>18}".format(
-                stdout_conf_dict[conf_dict["color_mapping"][selected_up_down_mask[_]]]
+            "{:>23}".format(
+                style
+                + stdout_conf_dict[conf_dict["color_mapping"][selected_up_down_mask[_]]]
                 + str(x)
                 + stdout_conf_dict["reset"]
             )
             for _, x in selected_row_dict.items()
         ),
-        end=row_position,
+        end="\n",
     )
 
 
@@ -42,17 +52,21 @@ def print_table(
     conf_dict: dict,
     up_down_mask: list,
     row_dict: dict,
+    row_count: int,
 ):
     """"""
     for row_num in conf_dict["symbol_dict"].keys():
+        row_int = int(row_num.replace("row_", ""))
         print_row(
-            " " * 17,
+            snapshot_time,
             stdout_conf_dict,
             conf_dict,
             up_down_mask,
             {x: x for x in row_dict.keys()},
             conf_dict["symbol_dict"][row_num],
-            "\n",
+            row_int * 2 - 1,
+            row_count,
+            stdout_conf_dict["bold"],
         )
         print_row(
             snapshot_time,
@@ -61,8 +75,11 @@ def print_table(
             up_down_mask,
             row_dict,
             conf_dict["symbol_dict"][row_num],
-            "\n",
+            row_int * 2,
+            row_count,
+            stdout_conf_dict["reset"],
         )
+    print("#" * 90)
 
 
 def watch_market(interval: int = 2):
@@ -73,10 +90,8 @@ def watch_market(interval: int = 2):
     data = pkgutil.get_data(__name__, "stdout_conf.yaml")
     stdout_conf_dict = yaml.safe_load(data)
 
-    with open("watch_market.yaml", "r") as f:
-        conf_dict = yaml.safe_load(f)
-    with open("stdout_conf.yaml", "r") as f:
-        stdout_conf_dict = yaml.safe_load(f)
+    row_count = int(list(conf_dict["symbol_dict"].keys())[-1].replace("row_", "")) * 2
+    print(" Watch Market ".center(90, "#"), "\n" * (row_count + 1))
 
     symbol_list = []
     for row in conf_dict["symbol_dict"].values():
@@ -89,32 +104,45 @@ def watch_market(interval: int = 2):
 
     session = requests.Session()
     while True:
-        resp = session.get(
-            url=conf_dict["yahoo_finance_api_entry"]
-            .replace("replace_field", field_string)
-            .replace("replace_symbol", symbol_string)
-        )
-        snapshot_time = str(
-            datetime.datetime.fromtimestamp(
-                resp.json()["quoteResponse"]["result"][0]["regularMarketTime"]
-            ).strftime("%Y%m%d %H:%M:%S")
-        )
-        snapshot_dict[0] = snapshot_dict[1]
-        snapshot_dict[1] = {
-            x["symbol"]: x["regularMarketPrice"]
-            for x in resp.json()["quoteResponse"]["result"]
-        }
-        for _ in symbol_list:
-            diff = snapshot_dict[1][_] - snapshot_dict[0][_]
-            if diff > 0:
-                up_down_mask[_] = 1
-            elif diff < 0:
-                up_down_mask[_] = -1
-            else:
-                up_down_mask[_] = 0
+        try:
+            resp = session.get(
+                url=conf_dict["yahoo_finance_api_entry"]
+                .replace("replace_field", field_string)
+                .replace("replace_symbol", symbol_string)
+            )
+            snapshot_time = str(
+                datetime.datetime.fromtimestamp(
+                    resp.json()["quoteResponse"]["result"][0]["regularMarketTime"]
+                ).strftime("%Y%m%d %H:%M:%S")
+            )
+            snapshot_dict[0] = snapshot_dict[1]
+            snapshot_dict[1] = {
+                x["symbol"]: x["regularMarketPrice"]
+                for x in resp.json()["quoteResponse"]["result"]
+            }
+            for _ in symbol_list:
+                diff = snapshot_dict[1][_] - snapshot_dict[0][_]
+                if diff > 0:
+                    up_down_mask[_] = 1
+                elif diff < 0:
+                    up_down_mask[_] = -1
+                else:
+                    up_down_mask[_] = 0
+        except KeyError:
+            print(
+                stdout_conf_dict["line_start"]
+                + "1A"
+                + stdout_conf_dict["line_clear"]
+                + "!!! Yahoo API fetch fail !!!"
+            )
 
         print_table(
-            snapshot_time, stdout_conf_dict, conf_dict, up_down_mask, snapshot_dict[1],
+            snapshot_time,
+            stdout_conf_dict,
+            conf_dict,
+            up_down_mask,
+            snapshot_dict[1],
+            row_count,
         )
 
         time.sleep(interval)
