075f5141bfac845ae12b70fcf414213791eb4755
wxianxin <wxianxinreg@gmail.com>
2024-09-15 07:46:58 -0400

    update

--- quantool/plot/plot.py
+++ quantool/plot/plot.py
@@ -45,12 +45,11 @@ def plot_dict_of_vec(data, title="Loss over Epochs", xlabel="Epoch", ylabel="Los
 def plot_performance(
     x,
     y,
-    drawdown,
-    position,
+    position=None,
     benchmark=None,
     title="Performance Metrics",
     xlabel="time",
-    ylabel="pnl",
+    ylabel="return",
 ):
     color = "palegreen"
     # downsample the data for better visualization
@@ -58,10 +57,16 @@ def plot_performance(
         step = len(x) // 10_000
         x = x[::step]
         y = y[::step]
-        drawdown = drawdown[::step]
-        position = position[::step]
+        if position is not None:
+            position = position[::step]
         if benchmark is not None:
             benchmark = benchmark[::step]
+
+    # calculate the nav, drawdown, volatility and sharpe ratio
+    nav = 1 + y[-1]
+    drawdown = y - np.maximum.accumulate(y)
+    vol = np.std(y)
+    sharpe = np.mean(y) / vol
 
     # Create the figure and the grid layout
     fig = plt.figure(figsize=(16, 9))
@@ -69,10 +74,10 @@ def plot_performance(
 
     # Main plot (top left)
     ax = fig.add_subplot(gs[0, 0])
-    ax.plot(x, y, "b")
+    ax.plot(x, y, "b", label="Return")
     if benchmark is not None:
         ax.plot(x, benchmark, "r", label="Benchmark")
-        ax.legend()
+    ax.legend()
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
@@ -94,6 +99,8 @@ def plot_performance(
     ax_drawdown.set_ylim(-y_max, 0)  # Set the drawdown y-axis limits to mirror main
 
     # Bottom plot (bottom left)
+    if position is None:
+        position = np.zeros_like(x)
     ax_1_0 = fig.add_subplot(gs[1, 0], sharex=ax)
     ax_1_0.plot(x, position, color)
     ax_1_0.set_ylabel("position")
@@ -108,13 +115,13 @@ def plot_performance(
         spine.set_visible(False)
 
     cell_text = [
-        ["NAV:", "101.23"],
-        ["Max DD:", "-10.50%"],
-        ["Sharpe:", "1.45"],
+        ["NAV:", "{:.2%}".format(nav)],
+        ["Max DD:", "{:.2%}".format(min(drawdown))],
+        ["Volatility:", "{:.2%}".format(vol)],
+        ["Sharpe:", "{:.2%}".format(sharpe)],
     ]
     table = ax_0_1.table(
         cellText=cell_text,
-        cellColours=[["red", "red"], ["green", "green"], ["blue", "blue"]],
         cellLoc="left",
         colWidths=[0.6, 0.4],
         loc="center",
